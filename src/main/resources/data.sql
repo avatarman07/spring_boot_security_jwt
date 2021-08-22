bd1ef51c9114eca4ef76615d93a8579ee35113f3
aron <aron@MacBook-Pro-de-Macbook.local>
2021-08-22 00:03:09 -0500

    modifico script para inicializar enums en mysql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,3 @@
-INSERT INTO `testdb`.`roles`
-(
-    `name`)
-VALUES
-    (
-        'ROLE_USER');
-
-INSERT INTO `testdb`.`roles`
-(
-    `name`)
-VALUES
-    (
-        'ROLE_MODERATOR');
-
-INSERT INTO `testdb`.`roles`
-(
-    `name`)
-VALUES
-    (
-        'ROLE_ADMIN');
+INSERT INTO testdb.roles(name) VALUES ('ROLE_USER');
+INSERT INTO testdb.roles(name) VALUES ('ROLE_MODERATOR');
+INSERT INTO testdb.roles(name) VALUES ('ROLE_ADMIN');
